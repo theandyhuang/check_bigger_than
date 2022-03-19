11ee6b85797de16532be216a3b02c78bee05d717
Andy Huang <andy2244759@gmail.com>
2022-03-19 14:01:07 +0800

    for loop version

--- bigger.py
+++ bigger.py
@@ -1,9 +1,7 @@
 number = int(input("please enter a number: "))
-i = 0
 sum1 = 0
-while True:
+for i in range(number):
     sum1 += i
     if sum1 >= number:
         break
-    i += 1
 print(f"Summation from 1 to {i} is {sum1} and that is over or equal to {number}")
